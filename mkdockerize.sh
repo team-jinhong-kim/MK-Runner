ba83e79496e198fd6809b299e22589c017807a7b
team-jinnhong-kim <team.jinhong.kim@gmail.com>
2020-11-02 01:15:12 +0900

    add comments on mkdockerize.sh

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -2,15 +2,15 @@
 
 Manual() {
   echo "run example"
-  echo "create Mkdocs project: docker run <arguments> <docker-image-name> produce <output path(.tar.gz)>"
-  echo "docker run -p 8000:8000 <arguments> <docker-image-name> serve"
+  echo "create Mkdocs project: docker run <arguments> <docker-image-name> produce > <output path(.tar.gz)>"
+  echo "cat .tar.gz | docker run -p 8000:8000 <arguments> <docker-image-name> serve"
 }
-
+# If command is produce
 if [[ $1 == 'produce' ]];
 then
 
   cd $MKPATH
-
+  # Confure dev_addr ip and port to allow host connect mkdocs service
   if ! grep -q 'dev_addr: 0.0.0.0:8000' mkdocs.yml;
   then
     echo 'dev_addr: 0.0.0.0:8000' >> mkdocs.yml
@@ -18,14 +18,20 @@ then
 
   mkdocs build --quiet
   cd ..
+  # If .tar.gz  file already exists, remove it
   if [[ -f ".tar.gz" ]];
   then
     rm .tar.gz
   fi
+
+  # Zip the archived mkdocs directory
   tar -zcvf .tar.gz -C $MKPATH . &> /dev/null
   rm -rf $MKPATH/site
+  # Write out .tar.gz file to stdout
   cat .tar.gz
 
+# If command is serve, receive .tar.gz file via stdin.
+# decompress, dearchive .tar.gz file then run mkdocs service
 elif [[ $1 == 'serve' ]];
 then
 
