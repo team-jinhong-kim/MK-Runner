b9319eb1f40149bbb273dc37e72e7a889041c908
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2020-11-01 23:40:30 +0900

    remove useless .sh files

--- build.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run -v /Users/jinhongkim/projects/MK-Runner/project:/app/project -e MKPATH=/app/project test produce > ./project/.tar.gz
--- serve.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run -v /Users/jinhongkim/projects/MK-Runner/project:/app/project -e MKPATH=/app/project test produce | docker run -p 8000:8000 -v /Users/jinhongkim/projects/MK-Runner/project:/app/project -e MKPATH=/app/project -i test serve
